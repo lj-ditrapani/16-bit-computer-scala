1a4404b9ee6c90e2e71493d6b2663a5f16dee2fc
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-05-07 19:15:04 -0400

    Add ByteProcessor specs

--- src/test/scala/bin-file-reader-spec.scala
+++ src/test/scala/bin-file-reader-spec.scala
@@ -5,6 +5,44 @@ import org.scalatest.{FunSpec, Matchers}
 class BinFileReaderSpec extends FunSpec with Matchers {
 }
 
+class ByteProcessorSpec extends FunSpec with Matchers {
+  describe("ByteProcessor class") {
+    def process(byte_array: Array[Byte]): BinFileReader.IfChars = {
+      new BinFileReader.ByteProcessor(byte_array).process()
+    }
+
+    describe("process") {
+      it("returns Left if byte_array is empty") {
+        process(Array()) should === (Left("binary file must not be empty"))
+      }
+
+      it("returns Left if byte_array is greater than 256 KB") {
+        val a: Array[Byte] = Array.fill[Byte](256 * 1024 + 1)(0)
+        process(a) match {
+          case Left(msg) => msg should include ("less than or equal to 256")
+        }
+      }
+
+      it("returns Left if byte_array has odd number of bytes") {
+        process(Array(0x61)) match {
+          case Left(msg) => msg should include ("even number of bytes")
+        }
+      }
+
+      it("returns Right if 256 KB byte_array passes all checks") {
+        val a: Array[Byte] = Array.fill[Byte](256 * 1024)(0)
+        process(a) match { case Right(_) => Unit }
+      }
+
+      it("returns Right if byte_array passes all checks") {
+        process(Array(0x61, 0x62)) match {
+          case Right(Array(x)) => x should === (24930.toChar)
+        }
+      }
+    }
+  }
+}
+
 class BytesCheckSpec extends FunSpec with Matchers {
   val fail_check = BinFileReader.Fail("foo")
 
