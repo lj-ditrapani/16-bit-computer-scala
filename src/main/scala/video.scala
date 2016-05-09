e5791f96470564063feda42c284da69b5baf9ae9
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-05-09 18:18:33 -0400

    Add VideoRam class & video subtypes

--- src/main/scala/video.scala
+++ src/main/scala/video.scala
@@ -17,6 +17,9 @@ case class Video(video_ram: Vector[Char], enable: Boolean, custom_tiles: Boolean
 
 object Video {
   type VideoBuffer = Vector[Vector[Color]]
+  type LargeTile = Vector[Vector[(Boolean, Boolean)]]
+  type SmallTile = Vector[Vector[(Boolean, Boolean)]]
+  type TextCharTile = Vector[Vector[Boolean]]
 
   def make(ram: Vector[Char]): Video = {
     val enable_bits = ram(0xDDF3)
@@ -30,4 +33,45 @@ object Video {
     (for (i <- 0 until 240) yield {
       (for (j <- 0 until 256) yield Color.rgb(0, 0, 0)).to[Vector]
     }).to[Vector]
+
+  case class VideoRam(
+    large_tiles: Vector[LargeTile],
+    small_tiles: Vector[SmallTile],
+    text_char_tiles: Vector[TextCharTile],
+    bg_cells: Vector[Vector[BgCell]],
+    text_cells: Vector[Vector[TextCell]],
+    bg_colors: Vector[(Color8, Color8)],
+    fg_colors: Vector[(Color8, Color8)],
+    large_sprites: Vector[Sprite],
+    small_sprites: Vector[Sprite]
+  ) {
+    def buffer: VideoBuffer = Vector(Vector())
+  }
+
+  case class BgCell(
+    color_pair_1: Byte,
+    color_pair_2: Byte,
+    x_flip: Boolean,
+    y_flip: Boolean,
+    large_tile_index: Byte
+  )
+
+  case class TextCell(
+    text_char_tile_index: Byte,
+    on: Boolean
+  )
+
+  case class Sprite(
+    color_pair_1: Byte,
+    color_pair_2: Byte,
+    x_flip: Boolean,
+    y_flip: Boolean,
+    tile_index: Byte,
+    x_position: Byte,
+    y_position: Byte,
+    on: Boolean
+  )
+
+  class Color8 {
+  }
 }
