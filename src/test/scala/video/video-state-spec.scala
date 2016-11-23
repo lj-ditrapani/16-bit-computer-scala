e3aab44541a283eafddc764a8ef45affcc7677c3
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-11-23 09:21:50 -0500

    Finish VideoState.buffer spec

--- src/test/scala/video/video-state-spec.scala
+++ src/test/scala/video/video-state-spec.scala
@@ -31,11 +31,16 @@ class VideoStateSpec extends Spec {
     describe("buffer") {
       it("returns a buffer with pixel colors set correctly") {
         val cells =
-          Vector(0x0FFF.toChar) ++        // black/white 256th cell
-          Vector.fill(638)(1.toChar) ++
-          Vector(0x42FE.toChar)           // blue/red 255th cell
+          Vector(0x01FF.toChar) ++            // black/white 256th tile
+          Vector.fill(638)(0xC901.toChar) ++  // dark green/light green 2nd tile
+          Vector(0x42FE.toChar)               // blue/red 255th tile
         val buffer = VideoState.make(cells, colors, tiles).buffer
         buffer(0)(0) shouldBe Color.rgb(0, 0, 0)
+        buffer(1)(0) shouldBe Color.rgb(255, 255, 255)
+        buffer(2)(8) shouldBe Color.rgb(0, 0x49, 0)
+        buffer(2)(9) shouldBe Color.rgb(0x92, 255, 0xAA)
+        buffer(235)(248) shouldBe Color.rgb(0, 0, 255)
+        buffer(235)(249) shouldBe Color.rgb(255, 0, 0)
       }
     }
   }
