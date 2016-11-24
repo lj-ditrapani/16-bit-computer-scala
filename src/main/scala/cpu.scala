e82880b0cbff2b08c6e79bd4414b37821345e9a0
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-11-24 18:03:57 -0500

    Implement BitUtils.getNibbles

--- src/main/scala/cpu.scala
+++ src/main/scala/cpu.scala
@@ -39,3 +39,13 @@ final class CpuAndRam(cpu: Cpu, ramSnapshot: Vector[Char]) {
 
   def getCpu: Cpu = Cpu(instruction_counter, Registers(registers.toVector), cpu.rom)
 }
+
+object BitUtils {
+  def getNibbles(word: Char): (Int, Int, Int, Int) = {
+    val op_code = word >> 12
+    val a = word >> 8 & 0xF
+    val b = word >> 4 & 0xF
+    val c = word & 0xF
+    (op_code, a, b, c)
+  }
+}
--- src/test/scala/cpu-spec.scala
+++ src/test/scala/cpu-spec.scala
@@ -26,3 +26,15 @@ class CpuSpec extends Spec {
 
 class CpuAndRamSpec extends Spec {
 }
+
+class BitUtilsSpec extends Spec {
+  describe("getNibbles") {
+    it("splits a word into 4 4-bit nibbles") {
+      BitUtils.getNibbles(0xABCD.toChar).shouldBe((0xA, 0xB, 0xC, 0xD))
+    }
+
+    it("splits another word into 4 4-bit nibbles") {
+      BitUtils.getNibbles(0x7712.toChar).shouldBe((0x7, 0x7, 0x1, 0x2))
+    }
+  }
+}
