e655bf76a8071fa353ac49e4cbe742623a217c58
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-11-25 15:49:52 -0500

    Refactor HBY & LBY specs

--- src/test/scala/cpu/executor-spec.scala
+++ src/test/scala/cpu/executor-spec.scala
@@ -10,57 +10,47 @@ class ExecutorSpec extends Spec {
   }
 
   describe("set byte operations") {
-    trait SetByteFixture {
-      def operation(executor: Executor, immediate: Int, register: Int): Unit
-
-      def testSetByteOperation(tests: List[(Int, Int, Int, Int)]): Unit = {
-        for (test <- tests) {
-          val (immediate, register, current_value, final_value) = test
-          it(s"sets R${register} to ${final_value}") {
-            new Fixture {
-              registers(register) = current_value.toChar
-              operation(executor, immediate, register)
-              registers(register) shouldBe final_value
-            }
+    def testSetByteOperation(
+        tests: List[(Int, Int, Int, Int)],
+        operation: (Executor, Int, Int) => Unit): Unit = {
+      for (test <- tests) {
+        val (immediate, register, current_value, final_value) = test
+        it(s"sets R${register} to ${final_value}") {
+          new Fixture {
+            registers(register) = current_value.toChar
+            operation(executor, immediate, register)
+            registers(register) shouldBe final_value
           }
         }
       }
     }
 
-    trait HbyFixture extends SetByteFixture {
+    describe("hby") {
+      val tests = List(
+        (0x05, 0x0, 0x0000, 0x0500),
+        (0x00, 0x3, 0xFFFF, 0x00FF),
+        (0xEA, 0xF, 0x1234, 0xEA34)
+      )
+
       def operation(executor: Executor, immediate: Int, register: Int): Unit = {
         executor.hby(immediate, register)
       }
-    }
 
-    trait LbyFixture extends SetByteFixture {
-      def operation(executor: Executor, immediate: Int, register: Int): Unit = {
-        executor.lby(immediate, register)
-      }
-    }
-
-    describe("hby") {
-      new HbyFixture {
-        val tests = List(
-          (0x05, 0x0, 0x0000, 0x0500),
-          (0x00, 0x3, 0xFFFF, 0x00FF),
-          (0xEA, 0xF, 0x1234, 0xEA34)
-        )
-
-        testSetByteOperation(tests)
-      }
+      testSetByteOperation(tests, operation)
     }
 
     describe("lby") {
-      new LbyFixture {
-        val tests = List(
-          (0x05, 0x0, 0x0000, 0x0005),
-          (0x00, 0x3, 0xFFFF, 0xFF00),
-          (0xEA, 0xF, 0x1234, 0x12EA)
-        )
+      val tests = List(
+        (0x05, 0x0, 0x0000, 0x0005),
+        (0x00, 0x3, 0xFFFF, 0xFF00),
+        (0xEA, 0xF, 0x1234, 0x12EA)
+      )
 
-        testSetByteOperation(tests)
+      def operation(executor: Executor, immediate: Int, register: Int): Unit = {
+        executor.lby(immediate, register)
       }
+
+      testSetByteOperation(tests, operation)
     }
   }
 }
