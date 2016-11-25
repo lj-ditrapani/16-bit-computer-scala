77936c277b4ed1152a6a50f69a6fb6ce9440b752
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-11-24 19:08:06 -0500

    Add method signatures for each CPU instruction

--- src/main/scala/cpu.scala
+++ src/main/scala/cpu.scala
@@ -28,16 +28,70 @@ final class CpuAndRam(cpu: Cpu, ramSnapshot: Vector[Char]) {
   @tailrec
   def run(n: Int): Unit = {
     if (n > 0) {
-      step()
-      run(n - 1)
+      val keep_going = step()
+      if (keep_going) {
+        run(n - 1)
+      }
     }
   }
 
-  def step(): Boolean = true
+  def step(): Boolean = {
+    val (op_code, a, b, c) = BitUtils.getNibbles(ram(instruction_counter.toInt))
+    val immd8 = a << 8 | b
+    op_code match {
+      case 0x0 => false
+      case 0x1 => hby(immd8, c)
+      case 0x2 => lby(immd8, c)
+      case 0x3 => lod(a, c)
+      case 0x4 => str(a, b)
+      case 0x5 => add(a, b, c)
+      case 0x6 => sub(a, b, c)
+      case 0x7 => adi(a, b, c)
+      case 0x8 => sbi(a, b, c)
+      case 0x9 => and(a, b, c)
+      case 0xA => orr(a, b, c)
+      case 0xB => xor(a, b, c)
+      case 0xC => not(a, c)
+      case 0xD => shf(a, b, c)
+      case 0xE => brv(a, b, c)
+      case 0xF => brf(b, c)
+      case _ => true
+    }
+  }
 
   def ramAsVector: Vector[Char] = ram.toVector
 
   def getCpu: Cpu = Cpu(instruction_counter, Registers(registers.toVector), cpu.rom)
+
+  def hby(immd8: Int, c: Int): Boolean = true
+
+  def lby(immd8: Int, c: Int): Boolean = true
+
+  def lod(rs1: Int, rd: Int): Boolean = true
+
+  def str(rs1: Int, rs2: Int): Boolean = true
+
+  def add(rs1: Int, rs2: Int, rd: Int): Boolean = true
+
+  def sub(rs1: Int, rs2: Int, rd: Int): Boolean = true
+
+  def adi(rs1: Int, rs2: Int, rd: Int): Boolean = true
+
+  def sbi(rs1: Int, rs2: Int, rd: Int): Boolean = true
+
+  def and(rs1: Int, rs2: Int, rd: Int): Boolean = true
+
+  def orr(rs1: Int, rs2: Int, rd: Int): Boolean = true
+
+  def xor(rs1: Int, rs2: Int, rd: Int): Boolean = true
+
+  def not(rs1: Int, rd: Int): Boolean = true
+
+  def shf(rs1: Int, da: Int, rd: Int): Boolean = true
+
+  def brv(rs1: Int, rs2: Int, cond_v: Int): Boolean = true
+
+  def brf(rs2: Int, cond_f: Int): Boolean = true
 }
 
 object BitUtils {
