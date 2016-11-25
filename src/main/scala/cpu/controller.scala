3a0a781ea8a8b72d9f40d3bf648bdfcd21250dd7
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-11-25 15:21:00 -0500

    Refactor/re-organize Controller.step()

--- src/main/scala/cpu/controller.scala
+++ src/main/scala/cpu/controller.scala
@@ -17,38 +17,26 @@ final class Controller(cpu: Cpu, ramSnapshot: Vector[Char]) {
     }
   }
 
-  def step(): Boolean = {
+  def ramAsVector: Vector[Char] = executor.getRam.toVector
+
+  def getCpu: Cpu = Cpu(
+    instruction_counter, Registers(executor.getRegisters.toVector), cpu.rom
+  )
+
+  private def step(): Boolean = {
     val (op_code, a, b, c) = BitUtils.getNibbles(
       cpu.rom(instruction_counter.toInt)
     )
-    val immd8 = a << 8 | b
     op_code match {
       case 0x0 => false
       case _ => {
-        op_code match {
+        instruction_counter = op_code match {
           case x if x < 0xE => {
-            op_code match {
-              case 0x1 => executor.hby(immd8, c)
-              case 0x2 => executor.lby(immd8, c)
-              case 0x3 => executor.lod(a, c)
-              case 0x4 => executor.str(a, b)
-              case 0x5 => executor.add(a, b, c)
-              case 0x6 => executor.sub(a, b, c)
-              case 0x7 => executor.adi(a, b, c)
-              case 0x8 => executor.sbi(a, b, c)
-              case 0x9 => executor.and(a, b, c)
-              case 0xA => executor.orr(a, b, c)
-              case 0xB => executor.xor(a, b, c)
-              case 0xC => executor.not(a, c)
-              case 0xD => executor.shf(a, b, c)
-            }
-            instruction_counter = (instruction_counter + 1).toChar
+            doNormalInstruction(op_code, a, b, c)
+            (instruction_counter + 1).toChar
           }
           case _ => {
-            instruction_counter = op_code match {
-              case 0xE => executor.brv(a, b, c)
-              case 0xF => executor.brf(b, c)
-            }
+            doBranchingInstruction(op_code, a, b, c)
           }
         }
         true
@@ -56,9 +44,30 @@ final class Controller(cpu: Cpu, ramSnapshot: Vector[Char]) {
     }
   }
 
-  def ramAsVector: Vector[Char] = executor.getRam.toVector
+  // scalastyle:off cyclomatic.complexity
+  private def doNormalInstruction(op_code: Int, a: Int, b: Int, c: Int): Unit = {
+    val immd8 = a << 8 | b
+    op_code match {
+      case 0x1 => executor.hby(immd8, c)
+      case 0x2 => executor.lby(immd8, c)
+      case 0x3 => executor.lod(a, c)
+      case 0x4 => executor.str(a, b)
+      case 0x5 => executor.add(a, b, c)
+      case 0x6 => executor.sub(a, b, c)
+      case 0x7 => executor.adi(a, b, c)
+      case 0x8 => executor.sbi(a, b, c)
+      case 0x9 => executor.and(a, b, c)
+      case 0xA => executor.orr(a, b, c)
+      case 0xB => executor.xor(a, b, c)
+      case 0xC => executor.not(a, c)
+      case 0xD => executor.shf(a, b, c)
+    }
+  }
+  // scalastyle:on cyclomatic.complexity
 
-  def getCpu: Cpu = Cpu(
-    instruction_counter, Registers(executor.getRegisters.toVector), cpu.rom
-  )
+  private def doBranchingInstruction(op_code: Int, rs1: Int, rs2: Int, cond: Int): Char =
+    op_code match {
+      case 0xE => executor.brv(rs1, rs2, cond)
+      case 0xF => executor.brf(rs2, cond)
+    }
 }
